d0a8fc47f4c3db6b2fc0f7917cb6622a80d50312
rishona <rishona>
2017-01-17 01:35:16 -0500

    progress to nyaa day settings +settings pane

--- options/options.js
+++ options/options.js
@@ -13,7 +13,7 @@ function main()
             
             getIds.forEach(function(e){
                 o=d2[e];
-                html+=genEntry(o.cover,o.title,o.link,ids[e],o.nyaa);
+                html+=genEntry(o.cover,o.title,o.link,ids[e],e,o.nyaa);
             });
 
             if (o.day==null)
@@ -24,16 +24,19 @@ function main()
             //daytable system not done
             dayTables[0].innerHTML=html;
             dayTables[0].parentElement.classList.remove("hidden");
-        });
-        
+
+            setNLinks();
+        });        
     });
 }
 
-function genEntry(cover,title,alistLink,progress,nyaa=0)
+function genEntry(cover,title,alistLink,progress,id,nyaa=0)
 {
+    var ntext="n";
     if (!nyaa)
     {
         nyaa="";
+        ntext="x";
     }
 
     else
@@ -45,6 +48,54 @@ function genEntry(cover,title,alistLink,progress,nyaa=0)
   <td class="cover"><img src="${cover}"></td>
   <td class="title"><a href="${alistLink}">${title}</a></td>
   <td class="ep button">${progress}</td>
-  <td class="nyaa button"><a ${nyaa}>n</a></td>
+  <td class="nyaa button"><a ${nyaa} data-id="${id}">${ntext}</a></td>
 </tr>`;
+}
+
+function setNLinks()
+{
+    var Nlinks=document.querySelectorAll(".nyaa a");
+    var sidePane=document.querySelector(".pane");
+    var paneSubmit=document.querySelector(".submitBox");
+    var paneCancel=document.querySelector(".cancelBox");
+    var paneTitle=document.querySelector(".pane-title");
+    var nyaa=document.querySelector(".nyaaInput");
+    var day=document.querySelector(".dayInput");
+    
+    Nlinks.forEach(function(e){
+        e.addEventListener("contextmenu",function(e2){
+            e2.preventDefault();
+
+            paneTitle.innerHTML=this.parentElement.parentElement.children[1].children[0].innerHTML;
+            paneSubmit.dataset.id=this.dataset.id;
+            sidePane.classList.remove("hidden");
+        });
+    });
+
+    paneSubmit.addEventListener("click",function(e){
+        updateND(this.dataset.id,nyaa.value,day.value);
+        nyaa.value="";
+        day.value=0;
+        sidePane.classList.add("hidden");
+    });
+
+    paneCancel.addEventListener("click",function(e){
+        nyaa.value="";
+        day.value=0;
+        sidePane.classList.add("hidden");        
+    });
+}
+
+function updateND(id,nyaa,day)
+{
+    chrome.storage.local.get(id,function(d){
+        var entry=d[id];
+
+        entry.nyaa=nyaa;
+        entry.day=day;
+
+        var setEntry={};
+        setEntry[id]=entry;
+        chrome.storage.local.set(setEntry);
+    });
 }
