dd038c2a776f97b14e1d33d0628877e270bc086e
risonakamo <hmykanae@gmail.com>
2019-01-25 16:54:26 -0500

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp=require("gulp");
+
+const plumber=require("gulp-plumber");
+
+const less=require("gulp-less");
+
+const babel=require("gulp-babel");
+
+console.log("gulp is watching");
+
+//remove any configs not being used
+var defaultPlumber=(err)=>{
+    console.log(err);
+};
+
+var lessConfig={
+    targets:[],
+    base:"."
+};
+
+var reactConfig={
+    targets:[],
+    base:"."
+};
+
+//remove any watchers not being used:
+
+//for less
+gulp.watch(lessConfig.targets).on("change",()=>{
+    gulp.src(lessConfig.targets,{base:lessConfig.base}).pipe(less()).pipe(gulp.dest(lessConfig.base));
+});
+
+//for react
+gulp.watch(reactConfig.targets).on("change",()=>{
+    gulp.src(reactConfig.targets,{base:reactConfig.base})
+        .pipe(plumber(defaultPlumber))
+        .pipe(babel({presets:["@babel/preset-react"],comments:false}))
+        .pipe(gulp.dest(reactConfig.base));
+});
