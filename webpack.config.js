cf8af0e947c64c05ae9ae56536c6530a47742b4c
risonakamo <hmykanae@gmail.com>
2020-10-18 18:02:40 -0400

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const MiniCssExtractPlugin=require("mini-css-extract-plugin");
+const ForkTsCheckerWebpackPlugin=require("fork-ts-checker-webpack-plugin");
+// const CopyPlugin=require("copy-webpack-plugin");
+const WebpackBar=require("webpackbar");
+
+module.exports={
+    mode:"development",
+    entry:{
+        // add entry points here
+        // index:"./index.tsx"
+    },
+    output:{
+        path:`${__dirname}/build`,
+        filename:"[name]-build.js"
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.(tsx|ts|js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets:["@babel/preset-react","@babel/preset-typescript"]
+                    }
+                }
+            },
+            {
+                test:/\.(less|css)$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
+                    {loader:"css-loader",options:{url:false}},
+                    {loader:"less-loader"}
+                ]
+            }
+        ]
+    },
+
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename:"[name]-build.css"
+        }),
+
+        new ForkTsCheckerWebpackPlugin(),
+        new WebpackBar()
+    ],
+
+    resolve:{
+        extensions:[".tsx",".ts",".jsx",".js"]
+    },
+
+    stats:{
+        entrypoints:false,
+        modules:false,
+        chunks:false
+    },
+
+    devtool:"cheap-module-source-map"
+};
