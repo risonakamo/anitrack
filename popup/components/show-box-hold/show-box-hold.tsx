e7b65f3809ff5fb764013c343849b4067dbe92e1
risonakamo <hmykanae@gmail.com>
2020-11-03 22:33:43 -0500

    show box hold styling

--- popup/components/show-box-hold/show-box-hold.tsx
+++ popup/components/show-box-hold/show-box-hold.tsx
@@ -2,10 +2,22 @@ import React from "react";
 
 import MiniShowBox from "../mini-show-box/mini-show-box";
 
+import "./show-box-hold.less";
+
 export default function ShowBoxHold():JSX.Element
 {
   return <div className="show-box-hold">
-    <MiniShowBox/>
-    <MiniShowBox/>
+    <div className="day-text-zone">
+      <p className="day-text">THU</p>
+    </div>
+    <div className="boxes">
+      <MiniShowBox/>
+      <MiniShowBox/>
+      <MiniShowBox/>
+      <MiniShowBox/>
+      <MiniShowBox/>
+      <MiniShowBox/>
+      <MiniShowBox/>
+    </div>
   </div>;
 }
