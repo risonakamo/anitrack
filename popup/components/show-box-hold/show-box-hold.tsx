a8e07dd95ea78f0539c4bfc539275c3b87a9b941
risonakamo <hmykanae@gmail.com>
2020-11-06 22:59:52 -0500

    open all nyaas of a day

--- popup/components/show-box-hold/show-box-hold.tsx
+++ popup/components/show-box-hold/show-box-hold.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import _ from "lodash";
 
 import MiniShowBox from "../mini-show-box/mini-show-box";
+import {openNyaa} from "../../../helpers/nyaa-helpers";
 
 import "./show-box-hold.less";
 
@@ -13,12 +14,24 @@ interface ShowBoxHoldProps
 
 export default function ShowBoxHold(props:ShowBoxHoldProps):JSX.Element
 {
+  // click handler for clicking day text. opens all nyaas of shows in the show hold.
+  function dayTextClick():void
+  {
+    for (var x=0;x<props.shows.length;x++)
+    {
+      if (props.shows[x].extras)
+      {
+        openNyaa(props.shows[x].extras!.nyaa);
+      }
+    }
+  }
+
   const shows:JSX.Element[]=_.map(props.shows,(x:CombinedShowInfo,i:number)=>{
     return <MiniShowBox key={i} show={x}/>;
   });
 
   return <div className="show-box-hold">
-    <div className="day-text-zone">
+    <div className="day-text-zone" onClick={dayTextClick}>
       <p className="day-text">
         {props.day}
         <span className="arrow">▸</span>
