c6745c9056dc7cf658f5f763f0306bb578b55322
risonakamo <hmykanae@gmail.com>
2020-11-13 22:49:28 -0500

    designed user configurator

--- showlist/components/user-configurator/user-configurator.tsx
+++ showlist/components/user-configurator/user-configurator.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import "./user-configurator.less";
+
 export default function UserConfigurator():JSX.Element
 {
   return <div className="user-configurator">
-    <input className="grey-line-input"/>
+    <span>Tracking User:</span>
+    <input className="grey-line-input config-input" placeholder="User"/>
+    <button>Apply</button>
   </div>
 }
