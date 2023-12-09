a54bf6de477f3a5daba586eb4f28af8b91782d0f
risonakamo <hmykanae@gmail.com>
2023-12-09 12:10:30 -0500

    content script working again

--- web/lib/anilist-hooks/inject-style-hook.ts
+++ web/lib/anilist-hooks/inject-style-hook.ts
@@ -1,5 +1,5 @@
 export function injectHookStyles():void
 {
     document.head.insertAdjacentHTML("beforeend",
-        `<link rel="stylesheet" href="${chrome.runtime.getURL("build/anilisthook-build.css")}">`);
+        `<link rel="stylesheet" href="${chrome.runtime.getURL("build2/style.css")}">`);
 }
